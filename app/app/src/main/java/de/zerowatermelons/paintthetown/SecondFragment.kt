720c4f772a9011d8c013300872c8a2a349b20b87
Fabian Nowak <58370948+FabianNowak@users.noreply.github.com>
2022-11-19 10:59:57 +0100

    Freies bewegen der kamera <-> fixierte kamera

--- app/app/src/main/java/de/zerowatermelons/paintthetown/SecondFragment.kt
+++ app/app/src/main/java/de/zerowatermelons/paintthetown/SecondFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.FreeCameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.viewport.state.ViewportState
+import com.mapbox.maps.plugin.viewport.viewport
 import de.zerowatermelons.paintthetown.databinding.FragmentSecondBinding
 import java.lang.ref.WeakReference
 
@@ -26,14 +34,35 @@ class SecondFragment : Fragment() {
     private lateinit var locationPermissionHelper: LocationPermissionHelper
 
     private lateinit var mapView: MapView
+    private lateinit var fab: FloatingActionButton
+
+    private var trackPosition: Boolean = true
+    private var point: Point? = null
 
     private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
-        mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+        if(trackPosition) {
+            //mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+        }
     }
 
     private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
-        mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
-        mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+        point = it
+        if(trackPosition) {
+            mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
+            mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+        }
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
     }
 
     private var _binding: FragmentSecondBinding? = null
@@ -53,6 +82,11 @@ class SecondFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fab = view.findViewById(R.id.fab)
+        fab.hide()
+        fab.setOnClickListener {
+            reenableCameraTracking()
+        }
         mapView = view.findViewById(R.id.mapView)
 
 
@@ -65,14 +99,14 @@ class SecondFragment : Fragment() {
     private fun onMapReady() {
         mapView.getMapboxMap().setCamera(
             CameraOptions.Builder()
-                .zoom(14.0)
+                .zoom(17.0)
                 .build()
         )
         mapView.getMapboxMap().loadStyleUri(
-            com.mapbox.maps.Style.MAPBOX_STREETS
+            com.mapbox.maps.Style.LIGHT
         ) {
             initLocationComponent()
-            //setupGesturesListener()
+            setupGesturesListener()
         }
     }
     private fun initLocationComponent() {
@@ -82,7 +116,7 @@ class SecondFragment : Fragment() {
             this.locationPuck = LocationPuck2D(
                 bearingImage = AppCompatResources.getDrawable(
                     this@SecondFragment.context!!,
-                    R.drawable.mapbox_user_puck_icon,
+                    R.drawable.mapbox_user_puck_icon2,
                 ),
                 scaleExpression = interpolate {
                     linear()
@@ -97,15 +131,40 @@ class SecondFragment : Fragment() {
                     }
                 }.toJson()
             )
+            this.pulsingEnabled = true
         }
         locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
         locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
     }
 
+    private fun setupGesturesListener() {
+        mapView.gestures.addOnMoveListener(onMoveListener)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun reenableCameraTracking() {
+
+        mapView.location.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.addOnMoveListener(onMoveListener)
+        if (point!=null) {
+            mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(point).build())
+        }
+        fab.hide()
+    }
+
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this.context, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+        fab.show()
     }
 
     override fun onRequestPermissionsResult(
