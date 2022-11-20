6bfe1afb7efebd61c74bab8a7aba252b55829405
Fabian Nowak <58370948+FabianNowak@users.noreply.github.com>
2022-11-20 04:52:56 +0100

    Visuelle verbesserungen

--- app/app/src/main/java/de/zerowatermelons/paintthetown/ArFragment.kt
+++ app/app/src/main/java/de/zerowatermelons/paintthetown/ArFragment.kt
@@ -10,11 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.filament.TextureSampler
+import com.google.android.filament.utils.rotation
+import com.google.android.filament.utils.scale
 import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.SceneView
+import com.google.ar.sceneform.collision.CollisionShape
 import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.*
@@ -32,7 +35,6 @@ const val STATUS = "winStatus"
 class ArFragment : Fragment(R.layout.fragment_ar) {
 
 
-
     private lateinit var arFragment: ArFragment
     private val arSceneView get() = arFragment.arSceneView
     private val scene get() = arSceneView.scene
@@ -46,28 +48,33 @@ class ArFragment : Fragment(R.layout.fragment_ar) {
     private var anchorNode: AnchorNode? = null
 
     private var SECTIONS = 32
-    private var SECTION_SIZE = 2*kotlin.math.PI / SECTIONS
+    private var SECTION_SIZE = 2 * kotlin.math.PI / SECTIONS
     private var pizza: BooleanArray = BooleanArray(SECTIONS)
     private var lastSlice: Int = -1
 
     private var n: Node? = null
 
-    inner class Bullet(var speed: Vector3) {
+    inner class Bullet(var speed: Vector3, var rotationImpulseDirection: Vector3, var rotationImpulseSpeed: Float) {
 
-        fun update(dt: Float){
+
+        fun update(dt: Float) {
             this.speed.y -= 9.81f * dt
-            if(this.speed.y < 0 && this.node.localPosition.y < -4f) {
+            if (this.speed.y < 0 && this.node.localPosition.y < -4f) {
                 this.done = true
                 this.node.localPosition = Vector3(node.localPosition.x, -4f, node.localPosition.z)
                 anchorNode!!.removeChild(this.node)
             }
 
-            this.node.localPosition = Vector3(this.node.localPosition.x + this.speed.x * dt, this.node.localPosition.y + this.speed.y * dt, this.node.localPosition.z + this.speed.z * dt)
-
+            this.node.localPosition = Vector3(
+                this.node.localPosition.x + this.speed.x * dt,
+                this.node.localPosition.y + this.speed.y * dt,
+                this.node.localPosition.z + this.speed.z * dt
+            )
+            this.node.localRotation = Quaternion.multiply(Quaternion.axisAngle(rotationImpulseDirection, rotationImpulseSpeed*dt), this.node.localRotation)
         }
 
-        lateinit var node : Node
-        var done : Boolean = false
+        lateinit var node: Node
+        var done: Boolean = false
 
         init {
             this.node = Node().apply {
@@ -78,18 +85,17 @@ class ArFragment : Fragment(R.layout.fragment_ar) {
                 var nx = speed.x / len
                 var nz = speed.z / len
 
-                localPosition = Vector3(nx, 0.0f , nz)
+                localPosition = Vector3(nx, 0.0f, nz)
 //                scaleController.minScale=0.001f
 //                scaleController.maxScale=999f
                 var randomFloat: Float = Random.nextFloat() * 0.4f;
-                localScale = Vector3(randomFloat, randomFloat,  randomFloat)
+                localScale = Vector3(randomFloat, randomFloat, randomFloat)
             }
             anchorNode!!.addChild(node)
             var mis = node.renderableInstance.filamentAsset!!.materialInstances
-            var ts = TextureSampler()
             for (mi in mis) {
                 mi.setColorWrite(true)
-                mi.setParameter("baseColorFactor", 0.0f, 0.0f, 1.0f)
+                mi.setParameter("baseColorFactor", 1.0f, 0.0f, 0.0f)
             }
         }
     }
@@ -118,17 +124,19 @@ class ArFragment : Fragment(R.layout.fragment_ar) {
 
     private suspend fun loadModels() {
         model = ModelRenderable.builder()
-            .setSource(context, Uri.parse("models/circle.glb"))
+            .setSource(context, Uri.parse("models/splat.glb"))
             .setIsFilamentGltf(true)
             .await()
+        model!!.collisionShape = null
+
         sModel = ModelRenderable.builder()
-            .setSource(context, Uri.parse("models/sphere.glb"))
+            .setSource(context, Uri.parse("models/egg.glb"))
             .setIsFilamentGltf(true)
             .await()
     }
 
     private fun onTapPlane(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
-        if (model == null || sModel==null) {
+        if (model == null || sModel == null) {
             Toast.makeText(context, "Loading...", Toast.LENGTH_SHORT).show()
             return
         }
@@ -151,7 +159,8 @@ class ArFragment : Fragment(R.layout.fragment_ar) {
             })
         })
     }
-private var cnt : Int = 0
+
+    private var cnt: Int = 0
     fun onUpdate(frameTime: FrameTime) {
         if (session == null) {
             session = arSceneView.session
@@ -178,7 +187,7 @@ private var cnt : Int = 0
             }
             scene.addChild(anchorNode)
         }
-        if(anchor == null)
+        if (anchor == null)
             return;
         //anchor's initialized from here on out
 
@@ -189,9 +198,9 @@ private var cnt : Int = 0
         if (index < 0)
             index = 0
         if (index >= SECTIONS)
-            index = SECTIONS-1
+            index = SECTIONS - 1
 
-        if(lastSlice == -1)
+        if (lastSlice == -1)
             lastSlice = index
 
         fireShotForRotation(index)
@@ -199,17 +208,17 @@ private var cnt : Int = 0
         val r1 = (SECTIONS - kotlin.math.max(index, lastSlice)) + kotlin.math.min(index, lastSlice)
         val r2 = kotlin.math.max(index, lastSlice) - kotlin.math.min(index, lastSlice)
         var dsum = 0;
-        if(r1 < r2){
-            for( i in  kotlin.math.max(index, lastSlice)  until SECTIONS){
+        if (r1 < r2) {
+            for (i in kotlin.math.max(index, lastSlice) until SECTIONS) {
                 fireShotForRotation(i)
                 dsum++
             }
-            for(i in 0.. kotlin.math.min(index, lastSlice)){
+            for (i in 0..kotlin.math.min(index, lastSlice)) {
                 fireShotForRotation(i)
                 dsum++
             }
-        }else{
-            for(i in kotlin.math.min(index, lastSlice)..kotlin.math.max(index, lastSlice)){
+        } else {
+            for (i in kotlin.math.min(index, lastSlice)..kotlin.math.max(index, lastSlice)) {
                 fireShotForRotation(i)
                 dsum++
             }
@@ -223,11 +232,11 @@ private var cnt : Int = 0
         //rot = Math.max(0.0, Math.min(2*Math.PI, rot));
 
         val bit = bullets.iterator()
-        while(bit.hasNext()){
+        while (bit.hasNext()) {
             val b = bit.next()
             b.update(frameTime.deltaSeconds)
             //println("CCCC " + b.)
-            if(b.done) {
+            if (b.done) {
                 genSplatFromBullet(b)
                 bit.remove()
             }
@@ -235,16 +244,16 @@ private var cnt : Int = 0
 
 
 
-        println("EEEE " + rot + ", "+ n?.localPosition?.x + ", " + n?.localPosition?.z)
+        println("EEEE " + rot + ", " + n?.localPosition?.x + ", " + n?.localPosition?.z)
         println("KKKK " + pizza.contentToString())
 
         //check if finished
         finishCheck()
     }
 
-    fun finishCheck(){
+    fun finishCheck() {
         for (slice in pizza) {
-            if(!slice)
+            if (!slice)
                 return
         }
         //finish
@@ -254,36 +263,46 @@ private var cnt : Int = 0
         //findNavController().navigate(R.id.action_ArFragment_to_SecondFragment, b)
     }
 
-    fun fireShotForRotation(index: Int){
-        if(pizza[index])
+    fun fireShotForRotation(index: Int) {
+        if (pizza[index])
             return
         println("CCCC " + pizza.asList())
         pizza[index] = true;
         val rot = index * SECTION_SIZE + 0.5f * SECTION_SIZE
-        println("LLLL" + index + " "+ rot)
+        println("LLLL" + index + " " + rot)
         val angle = rot.toFloat()
-        for(i in 1..10)
-            addChildSplatR(0f, -1.8f, -Random.nextDouble(1.0, 7.0).toFloat(),
-                angle + (Random.nextFloat() - 0.5f)*SECTION_SIZE.toFloat())
+        for (i in 1..8)
+            addChildSplatR(
+                0f, -1.8f, -Random.nextDouble(1.0, 7.0).toFloat(),
+                angle + (Random.nextFloat() - 0.5f) * SECTION_SIZE.toFloat()
+            )
     }
 
-    fun addChildSplatR(x: Float, y: Float, z: Float, angle: Float){
+    fun addChildSplatR(x: Float, y: Float, z: Float, angle: Float) {
         var xnew = translateX(x, z, angle)
         var znew = translateZ(x, z, angle)
         addChildSplat(xnew, y, znew)
     }
 
-    fun translateX(x: Float, z: Float, angle: Float) : Float{
+    fun translateX(x: Float, z: Float, angle: Float): Float {
         return x * kotlin.math.cos(angle) + z * kotlin.math.sin(angle)
     }
 
-    fun translateZ(x: Float, z: Float, angle: Float) : Float{
+    fun translateZ(x: Float, z: Float, angle: Float): Float {
         return x * -kotlin.math.sin(angle) + z * kotlin.math.cos(angle)
     }
 
     fun addChildSplat(x: Float, y: Float, z: Float) {
 
-        var b = Bullet(Vector3(2.0f*x, Random.nextDouble(3.0, 5.0).toFloat(), 2.0f*z))
+        var b = Bullet(
+            Vector3(2.0f * x, Random.nextDouble(3.0, 5.0).toFloat(), 2.0f * z),
+                Vector3(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat()
+                ).normalized(),
+            Random.nextFloat()*720
+        )
         //var b = Bullet(Vector3(0.0f, 5.0f, 50.0f))
         this.bullets.add(b)
     }
@@ -293,13 +312,14 @@ private var cnt : Int = 0
         // Create the transformable model and add it to the anchor.
         var c = Node().apply {
             renderable = model
-            renderableInstance.setCulling(false)
-            renderableInstance.animate(true).start()
+            renderableInstance.setCulling(true)
 
             localPosition = b.node.localPosition
 //            scaleController.minScale=0.001f
 //            scaleController.maxScale=999f
-            localScale = Vector3(b.node.localScale.x * 5.0f, 1f, b.node.localScale.z * 5.0f)
+            localScale = Vector3(b.node.localScale.x * 7.0f, 1f, b.node.localScale.z * 7.0f)
+            localRotation =
+                Quaternion.axisAngle(Vector3(0f, 1f, 0f), Random.nextDouble(0.0, 360.0).toFloat())
         }
         anchorNode!!.addChild(c)
         var mis = c.renderableInstance.filamentAsset!!.materialInstances
